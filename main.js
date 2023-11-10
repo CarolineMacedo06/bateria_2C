70c0728063ebde4d062e857fe074bca2a94fee9c
CarolineMacedo06 <130760650+CarolineMacedo06@users.noreply.github.com>
2023-11-10 09:35:38 -0300

    .

--- main.js
+++ main.js
@@ -1,4 +1,21 @@
+
+   <!DOCTYPE html>
+<html>
+<head>
+   <title>Hello, World!</title>
+</head>
+<body>
+
 function tocaSomPom (){
     document.querySelector('#som_tecla_pom').play();
     }
     document.querySelector('.tecla_pom') .onclick = tocaSomPom;
+
+
+<script type="text/javascript">
+   var doc = window.document;
+   alert( doc.title);    // alerts: Hello, World!
+</script>
+
+</body>
+</html>
